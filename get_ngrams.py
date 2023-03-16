d8ca1760f31ec9feadfbb3bb49a83c6ff1439a24
seandriscoll6 <sean.driscoll91@gmail.com>
2023-03-17 09:19:10 +1100

    clean up

--- get_ngrams.py
+++ get_ngrams.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     import dask.dataframe as dd
 
     df = dd.read_csv(output_filename, sep=',')
-
+    #test for github
     df = df.groupby(['Name']).sum().compute()
     df= df.sort_values(by=['Occurrence'], ascending=False)
     #df= df.head(10000)
